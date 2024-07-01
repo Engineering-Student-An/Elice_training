b6d890dd0c4d71d9707a9b7dc4bea83b476d670f
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-01 16:31:32 +0900

    ch 2-1. 퀴즈 확인

---
+++ src/week2/ch2_1/Test.java
@@ -0,0 +1,18 @@
+package week2.ch2_1;
+
+public class Test {
+    public static void main(String[] args) {
+//        Book book1 = new Book("A");
+//        Book book2 = book1;
+//        book2.title = "BB";
+
+//        System.out.println("book2.title = " + book2.title);
+    }
+}
+
+//class Book {
+//    String title;
+//    Book(String title) {
+//        this.title = title;
+//    }
+//}
