4abbd64c1547b40d8ce12c3a6f181f007d6e5fca
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-01 16:31:42 +0900

    ch 2-1. 실습1

---
+++ src/week2/ch2_1/Training1.java
@@ -0,0 +1,17 @@
+package week2.ch2_1;
+
+public class Training1 {
+    public static void main(String[] args) {
+
+        // 2번 지시사항을 참고해여 코드를 작성해 보세요.
+//        Book book = new Book();
+//        book.title = "자바의 정석";
+//        System.out.println(book.title);
+    }
+}
+
+// 1번 지시사항을 참고해여 코드를 작성해 보세요.
+//class Book {
+//    String title;
+//
+//}
