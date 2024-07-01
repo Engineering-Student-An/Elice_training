d617bd3a0241c2fd25c34a24d4459cb1667786be
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-01 16:31:45 +0900

    ch 2-1. 실습2

---
+++ src/week2/ch2_1/Training2.java
@@ -0,0 +1,24 @@
+package week2.ch2_1;
+
+
+// Order 클래스를 선언하고, 멤버 변수와 메서드(printOrderInfo)를 작성해 주세요.
+class Order {
+    public String menu;
+    public int price;
+
+    public void printOrderInfo() {
+        System.out.println("주문한 메뉴는 " + this.menu + ", 가격은 " + price + "원입니다.");
+    }
+}
+
+public class Training2 {
+    public static void main(String args[]) {
+
+        // 여기에 인스턴스를 생성하고 멤버 변수를 초기화합니다.
+        Order order = new Order();
+        order.menu = "아메리카노";
+        order.price = 4000;
+
+        order.printOrderInfo();
+    }
+}
