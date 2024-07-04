e0f45956e17d877f33adb401a3a02912a7321f90
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-04 16:29:30 +0900

    ch2-4. 실습 1

---
+++ src/week2/ch2_4/training1/Main.java
@@ -0,0 +1,16 @@
+package week2.ch2_4.training1;
+
+class Fruit { }
+
+class Banana extends Fruit { }
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 이 코드를 수정해 주세요.
+//        Banana banana = new Fruit();
+        Fruit fruit = new Banana();
+
+        System.out.println("상속 계층 관계 활용한 다형성 구현 성공!");
+    }
+}
