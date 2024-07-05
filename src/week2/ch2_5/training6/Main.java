0684b8c7bf5b4b12797248c4ffdadb5c128ca8d3
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-05 15:41:16 +0900

    ch2-5. 실습 6

---
+++ src/week2/ch2_5/training6/Main.java
@@ -0,0 +1,11 @@
+package week2.ch2_5.training6;
+
+public class Main {
+    public static void main(String args[]) {
+        Attackable ch1 = new Character("Attackable");
+        ItemUsable ch2 = new Character("ItemUsable");
+        SkillUsable ch3 = new Warrior("Warrior - SkillUsable");
+        SkillUsable ch4 = new Archer("Archer - SkillUsable");
+        SkillUsable ch5 = new Wizard("Wizard - SkillUsable");
+	}
+}
