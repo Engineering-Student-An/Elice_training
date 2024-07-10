fac90b8819e3470e31005030efd5a11d6263fabc
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-10 21:07:00 +0900

    ch3-3. Lambda Example

---
+++ src/week3/ch3_3/lambda/LambdaEx.java
@@ -0,0 +1,14 @@
+package week3.ch3_3.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaEx {
+
+    public static void main(String[] args) {
+
+        List<String> strings = Arrays.asList("a", "b", "c");
+//        strings.forEach(s -> System.out.println(s.toUpperCase()));
+
+    }
+}
