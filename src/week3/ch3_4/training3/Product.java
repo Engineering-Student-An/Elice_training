a856069d28f744afdaced606f39c477c25e6ec1a
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-11 15:45:44 +0900

    ch3-4. 실습 3

---
+++ src/week3/ch3_4/training3/Product.java
@@ -0,0 +1,19 @@
+package week3.ch3_4.training3;
+
+public class Product {
+    private String name;
+    private int stock;
+
+    public Product(String name, int stock) {
+        this.name = name;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+}
