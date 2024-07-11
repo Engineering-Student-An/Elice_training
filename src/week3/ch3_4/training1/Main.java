0dd0b698f09ab9436586efb1ca54e2344f8977be
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-11 15:45:27 +0900

    ch3-4. 실습 1

---
+++ src/week3/ch3_4/training1/Main.java
@@ -0,0 +1,24 @@
+package week3.ch3_4.training1;
+
+import java.util.Random;
+
+class RandomAdapter implements RandomProvider {
+    private final Random random = new Random();
+
+    @Override
+    public boolean nextBoolean() {
+        return random.nextBoolean();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Wizard wizard = new Wizard(new RandomAdapter());
+
+        try {
+            wizard.castSpell();
+        } catch (SpellException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/week3/ch3_4/training1/SpellException.java
@@ -0,0 +1,8 @@
+package week3.ch3_4.training1;
+
+// 지시 사항을 참고하여 코드를 작성해 보세요.
+class SpellException extends Exception {
+    public SpellException(String message) {
+        super(message);
+    }
+}
