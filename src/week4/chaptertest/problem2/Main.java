a05def8ecd25715ae1385ee47d6564fad5c652cf
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-22 11:42:42 +0900

    ch4. 챕터테스트 2

--- src/week4/chaptertest/problem2/Main.java
+++ src/week4/chaptertest/problem2/Main.java
@@ -13,10 +13,11 @@ public class Main {
         String filePath = System.getProperty("user.dir") + "/src/week4/chaptertest/problem2/Test.txt";
         try(FileOutputStream fileOutputStream = new FileOutputStream(filePath)) {
 
-            byte[] bytes = inputData.getBytes();
-            for (byte b : bytes) {
-                fileOutputStream.write(b);
-            }
+//            byte[] bytes = inputData.getBytes();
+//            for (byte b : bytes) {
+//                fileOutputStream.write(b);
+//            }
+            fileOutputStream.write(inputData.getBytes());
             System.out.println("파일에 데이터가 성공적으로 입력되었습니다.");
 
         } catch (FileNotFoundException e) {
