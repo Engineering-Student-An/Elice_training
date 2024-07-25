2b17ed8fd092143f45069b460c4805bdab27dd27
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-25 22:41:30 +0900

    ch5-4. 실습 12

---
+++ src/week5/ch5_4/training12.sql
@@ -0,0 +1,11 @@
+-- 아래는 EMPLOYEE 테이블에 대한 구조를 보여줍니다. 테이블을 수정하실 필요는 없습니다.
+DESC EMPLOYEE;
+
+-- 급여가 8000미만인 직원의 관리자 아이디와 급여를 출력하는 쿼리를 작성하세요.
+
+SELECT employee_id, salary
+FROM EMPLOYEE
+WHERE employee_id IN (SELECT manager_id
+                      FROM EMPLOYEE
+                      WHERE salary < 8000)
+ORDER BY employee_id ASC;
