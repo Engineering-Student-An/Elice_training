e84aa0e8624d5a6d0be4b85e9647709ca03ca838
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-07-31 11:50:09 +0900

    ch6-3. 실습 4

---
+++ src/week6/ch6_3/training4/UserService.java
@@ -0,0 +1,21 @@
+package week6.ch6_3.training4;
+
+import jakarta.annotation.*;
+import org.springframework.stereotype.*;
+
+// TODO: Bean 설정
+@Service
+public class UserService {
+    // TODO: 생성 메서드 구현
+    @PostConstruct
+    public void init() {
+        System.out.println("UserService Bean 생성!");
+    }
+    
+    // TODO: 파괴 메서드 구현
+    @PreDestroy
+    public void destroy() {
+        System.out.println("UserService Bean 파괴!");
+    }
+
+}
