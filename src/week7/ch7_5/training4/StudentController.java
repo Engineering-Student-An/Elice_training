b8c0f41cbe8ead8ece2f8e1eca9d9ca2cf32ebe2
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-08-09 16:04:35 +0900

    ch7-5. 실습 4

---
+++ src/week7/ch7_5/training4/StudentController.java
@@ -0,0 +1,42 @@
+package week7.ch7_5.training4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class StudentController {
+    private List<Student> students = new ArrayList<>();
+
+    public StudentController() {
+        students.add(new Student(1, "Alice", "alice@example.com", "2학년"));
+        students.add(new Student(2, "Bob", "bob@example.com", "3학년"));
+        students.add(new Student(3, "Charlie", "charlie@example.com", "1학년"));
+    }
+
+    @GetMapping("/students")
+    public String getStudents(@RequestParam(value = "grade", required = false) String grade, Model model) {
+        List<Student> filteredStudents = new ArrayList<>();
+
+        // 지시사항을 참고하여 코드를 작성해 보세요.
+        if(grade == null || grade.equals("모두")){
+            for(Student student : students) {
+                filteredStudents.add(student);
+            }
+        } else {
+            for(Student student : students) {
+                if(student.getGrade().equals(grade)) {
+                    filteredStudents.add(student);
+                }
+            }
+        }
+        
+        model.addAttribute("students", filteredStudents);
+        model.addAttribute("selectedGrade", grade);
+        return "students";
+    }
+}
