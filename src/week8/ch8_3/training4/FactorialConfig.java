d9c8d7130f4b246f0f9c4e014dc9b131900f4092
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-08-14 22:49:13 +0900

    ch8-3. 실습 4

---
+++ src/week8/ch8_3/training4/FactorialConfig.java
@@ -0,0 +1,29 @@
+package week8.ch8_3.training4;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class FactorialConfig {
+    
+    // 지시사항에 따라 코드를 작성해 보세요. 
+    @Bean
+    @Order(2)
+    public FactorialTimeAspect factorialTimeAspect() {
+        return new FactorialTimeAspect();
+    }
+
+    @Bean
+    @Order(1)
+    public FactorialLoggingAspect factorialLoggingAspect() {
+        return new FactorialLoggingAspect();
+    }
+
+    @Bean
+    public Factorial factorial() {
+        return new FactorialWithRecursion();
+    }
+}
