d0d8db8d0099bec24ae2ba3d1cf07deb864f129f
An-changmin <139744625+An-changmin@users.noreply.github.com>
2024-08-16 13:11:30 +0900

    ch8-5. 실습 7

---
+++ src/week8/ch8_5/training7/Department.java
@@ -0,0 +1,22 @@
+package week8.ch8_5.training7;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // 지시사항을 참고하여 코드를 작성해 보세요.
+    @OneToMany(mappedBy="department")
+    private List<Employee> employees = new ArrayList<>();
+}
