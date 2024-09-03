fd860de67d435c17a90cec7816ed0686c20f7346
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-09-03 10:43:11 +0900

    ch11-2. 실습 7

---
+++ src/week11/ch11_2/training7/Post.java
@@ -0,0 +1,36 @@
+package week11.ch11_2.training7;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "POST")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="post_id")
+    private Long id;
+
+    private String title;
+    private String content;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate;
+    
+    // TODO: 1. Post 클래스에 Image 리스트 객체를 참조하는 필드를 추가하세요.   
+    @OneToMany(mappedBy = "post")
+    private List<Image> images = new ArrayList<>();
+    
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
