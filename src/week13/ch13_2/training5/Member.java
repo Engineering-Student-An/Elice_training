ea2f4ecd26a8ea759ec2b633d1d4fead049c0cdb
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-09-24 14:31:46 +0900

    ch13-2. 실습 5 (Authorization)

---
+++ src/week13/ch13_2/training5/Member.java
@@ -0,0 +1,40 @@
+package week13.ch13_2.training5;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    private String name;
+    private String email;
+    private String password;
+
+    public Member(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Role{
+        USER("ROLE_USER", "일반 회원"),
+        ADMIN("ROLE_ADMIN", "관리자");
+
+        private final String key;
+        private final String title;
+    }
+}
