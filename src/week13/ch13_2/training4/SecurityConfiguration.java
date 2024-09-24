6e34bd38d4b5c53f902062b849193c8ac20c6c40
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-09-24 14:31:32 +0900

    ch13-2. 실습 4 (SecurityConfiguration 설정)

---
+++ src/week13/ch13_2/training4/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package week13.ch13_2.training4;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+// 지시사항을 참고하여 코드를 작성해 보세요.
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 지시사항을 참고하여 코드를 작성해 보세요.
+        http.csrf(csrf -> csrf.disable());
+        http.formLogin((auth) -> auth.loginPage("/login").loginProcessingUrl("/login").defaultSuccessUrl("/").failureUrl("/login-form?error").usernameParameter("name").passwordParameter("password").permitAll()); 
+        http.logout((auth) -> auth.logoutUrl("/logout").logoutSuccessUrl("/").permitAll());
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login", "/login-form").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+
+    }
+
+    // 테스트를 위한 코드입니다. 변경하지 않도록 합니다.
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("elicer")
+                .password("1234")
+                .roles("USER")
+                .build();
+
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
