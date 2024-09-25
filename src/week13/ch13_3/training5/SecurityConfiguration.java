5e5301fad177b16b089cbe1f23e13d1d7c8091a2
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-09-25 14:23:16 +0900

    ch13-3. 실습 5 (Remember Me 설정하기)

---
+++ src/week13/ch13_3/training5/SecurityConfiguration.java
@@ -0,0 +1,66 @@
+package week13.ch13_3.training5;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.elice.member.Member;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((authorizeRequests) ->
+                authorizeRequests
+                    .requestMatchers("/", "/login").permitAll()
+                    .requestMatchers("/admin/**").hasRole(Member.Role.ADMIN.name())
+                    .requestMatchers("/my-page/**").hasAnyRole(Member.Role.USER.name(), Member.Role.ADMIN.name())
+                    .anyRequest().permitAll()
+            )
+            .formLogin(formLogin -> formLogin
+                .loginPage("/login-form")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login-form?error")
+                .usernameParameter("name")
+                .passwordParameter("password")
+            )
+            .logout(logout -> logout
+                .logoutSuccessUrl("/")
+            )
+            // 지시사항을 참고하여 코드를 작성해 보세요.
+            .rememberMe(rememberMe -> rememberMe.key("eliceRememberMeKey").tokenValiditySeconds(60*60*24*7));
+        return http.build();
+
+    }
+
+    // 테스트를 위한 코드입니다. 변경하지 않도록 합니다.
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("elicer")
+                .password("1234")
+                .roles("USER")
+                .build();
+
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1234")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+}
