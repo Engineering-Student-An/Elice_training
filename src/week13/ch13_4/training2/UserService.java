e12fc985b410a9307a98f446277bbe1cda826405
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-09-26 15:36:56 +0900

    ch13-4. 실습 2 (토큰기반 인증 - OTP 생성코드 검증기능 구현하기)

---
+++ src/week13/ch13_4/training2/UserService.java
@@ -0,0 +1,94 @@
+package week13.ch13_4.training2;
+
+import com.elice.entities.Otp;
+import com.elice.entities.User;
+import com.elice.repository.OtpRepository;
+import com.elice.repository.UserRepository;
+import com.elice.utils.GenerateCodeUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final OtpRepository otpRepository;
+
+    public User addUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<User> auth(User user) {
+        Optional<User> o = userRepository.findUserByUsername(user.getUsername());
+
+        if(o.isPresent()) {
+            User u = o.get();
+            if (passwordEncoder.matches(user.getPassword(), u.getPassword())) {
+                renewOtp(u);
+            } else {
+                throw new BadCredentialsException("Bad credentials.");
+            }
+
+            return o;
+        } else {
+            throw new BadCredentialsException("Bad credentials.");
+        }
+    }
+
+    public boolean check(Otp otpToValidate) {
+        //지시사항을 참고하여 코드를 작성해 보세요.
+        Optional<Otp> userOtp = otpRepository.findOtpByUsername(otpToValidate.getUsername());
+        if(userOtp.isPresent()) {
+            Otp otp = userOtp.get();
+            return otpToValidate.getCode().equals(otp.getCode());
+        }
+
+        return false;
+    }
+
+    private void renewOtp(User u) {
+        String code = GenerateCodeUtil.generateCode();
+
+        Optional<Otp> userOtp = otpRepository.findOtpByUsername(u.getUsername());
+        if (userOtp.isPresent()) {
+            Otp otp = userOtp.get();
+            otp.setCode(code);
+        } else {
+            Otp otp = new Otp();
+            otp.setUsername(u.getUsername());
+            otp.setCode(code);
+            otpRepository.save(otp);
+        }
+    }
+
+    public String generateAndSaveOtp(String username) {
+        Optional<User> user = userRepository.findUserByUsername(username);
+        if (user.isPresent()) {
+            String code = GenerateCodeUtil.generateCode();
+
+            Optional<Otp> existingOtp = otpRepository.findOtpByUsername(username);
+            if (existingOtp.isPresent()) {
+                Otp otp = existingOtp.get();
+                otp.setCode(code);
+            } else {
+                Otp newOtp = new Otp();
+                newOtp.setUsername(username);
+                newOtp.setCode(code);
+                otpRepository.save(newOtp);
+            }
+
+            return code;
+        } else {
+            throw new BadCredentialsException("User not found.");
+        }
+    }
+}
