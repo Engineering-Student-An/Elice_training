8c69b8e15602a1823b4e837b6970adbff9f8c5e4
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-10-28 13:05:13 +0900

    ch18-1. 실습 6 (올바른 괄호 문자열)

---
+++ src/week18/ch18_1/training6.java
@@ -0,0 +1,25 @@
+package week18.ch18_1;
+
+import java.util.Scanner;
+
+public class training6 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.next();
+
+        int sum = 0;
+        boolean isCorrect = true;
+        for (int i = 0; i < string.length(); i++) {
+            if(string.charAt(i) == '(') sum += 1;
+            else sum -= 1;
+
+            if(sum < 0) {
+                isCorrect = false;
+                break;
+            }
+        }
+
+        System.out.println( (isCorrect && sum ==0) ? "YES" : "NO");
+    }
+}
