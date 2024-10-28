8259404ca20e1078cbd2bf3d3170aef3a121a7b3
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-10-28 13:04:41 +0900

    ch18-1. 실습 3 (최대공약수와 최소공배수)

---
+++ src/week18/ch18_1/training3.java
@@ -0,0 +1,33 @@
+package week18.ch18_1;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class training3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int gcd = gcd(n, m);
+        BigInteger lcm = BigInteger.valueOf(n).multiply(BigInteger.valueOf(m)).divide(BigInteger.valueOf(gcd));
+        System.out.println(gcd + " " + lcm);
+    }
+
+    public static int gcd(int n, int m) {
+
+        int max = Math.max(n, m);
+        int min = Math.min(n, m);
+
+        while(max % min != 0) {
+            max = max % min;
+
+            int temp = max;
+            max = min;
+            min = temp;
+        }
+
+        return min;
+    }
+}
