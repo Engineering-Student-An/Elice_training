4e2ef0875831a2cfe9ca4fe8e9577e73c29d24a6
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-10-28 13:05:03 +0900

    ch18-1. 실습 5 (회문 소수)

---
+++ src/week18/ch18_1/training5.java
@@ -0,0 +1,47 @@
+package week18.ch18_1;
+
+import java.util.Scanner;
+
+public class training5 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+
+            int str = scanner.nextInt();
+
+            if(isPrime(str) && isPalindrome(str)) {
+                count ++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    private static boolean isPrime(int str) {
+
+        int sqrt = (int) Math.sqrt(str);
+        if(sqrt * sqrt == str) return false;
+
+        for (int i = 2; i < sqrt; i++) {
+            if(str % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isPalindrome(int str) {
+        String string = String.valueOf(str);
+        for (int j = 0; j < string.length()/2; j++) {
+            if(string.charAt(j) != string.charAt(string.length() - 1 - j)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
