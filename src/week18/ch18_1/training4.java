a377aa934f18f5b1560d0602c5f420e73f3afae6
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-10-28 13:04:53 +0900

    ch18-1. 실습 4 (소수 수수께끼)

---
+++ src/week18/ch18_1/training4.java
@@ -0,0 +1,30 @@
+package week18.ch18_1;
+
+import java.util.Scanner;
+
+public class training4 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        boolean[] isPrime = new boolean[n+1];
+
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+
+            if(isPrime[i]) {
+                count ++;
+                for (int j = i*2; j <= n; j+=i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
