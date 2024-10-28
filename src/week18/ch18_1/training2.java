f316dc9f83517bfcc20af4dbce4cd433040616b7
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-10-28 13:04:28 +0900

    ch18-1. 실습 2 (약수의 개수 2)

---
+++ src/week18/ch18_1/training2.java
@@ -0,0 +1,35 @@
+package week18.ch18_1;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class training2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int t = scanner.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            BigInteger n = scanner.nextBigInteger();
+
+            boolean isSquare = false;
+            for(int j=1; j<=1000000; j++) {
+                if (n.equals(BigInteger.valueOf((long) j * j))) {
+                    isSquare = true;
+                    break;
+                }
+                if(BigInteger.valueOf((long) j * j).compareTo(n) > 0) break;
+            }
+
+            if(isSquare) {
+                sb.append("Odd\n");
+            } else {
+                sb.append("Even\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
