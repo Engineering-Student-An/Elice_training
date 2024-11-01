04f2de69d0bd99bd7c22af3743a514b24aec52af
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-11-01 11:13:37 +0900

    ch18-5. 실습 5 (소문)

---
+++ src/week18/ch18_5/training5/Main.java
@@ -0,0 +1,57 @@
+package week18.ch18_5.training5;
+
+import java.util.*;
+
+public class Main {
+    static ArrayList<Integer>[] adjacentList;
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        adjacentList = new ArrayList[n+1];
+        for (int i = 1; i <= n; i++) {
+            adjacentList[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = scanner.nextInt();
+            int v = scanner.nextInt();
+
+            adjacentList[u].add(v);
+            adjacentList[v].add(u);
+        }
+
+        boolean[] visit = new boolean[n+1];
+
+        bfs(1, visit);
+
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if(visit[i]) count++;
+        }
+
+        System.out.println(count);
+    }
+
+    private static void bfs(int v, boolean[] visit) {
+
+        visit[v] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(v);
+
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+            for (Integer i : adjacentList[poll]) {
+                if(!visit[i]) {
+                    queue.add(i);
+                    visit[i] = true;
+                }
+            }
+        }
+    }
+
+
+}
